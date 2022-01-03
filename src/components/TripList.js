aef6219d7ede9888d6dfd27afc74f5b86b88e812
Logan Willis <loganw0315@gmail.com>
2022-01-03 10:27:34 -0600

    Added error catch

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,30 +1,19 @@
-import { useCallback, useEffect, useState } from "react"
+import { useState } from "react"
+import { useFetch } from '../hooks/useFetch'
 
 import './TripList.css'
 
 export default function TripList() {
-    const [trips, setTrips] = useState([])
     const [url, setUrl] = useState('http://localhost:3000/trips')
+    const {data:trips, isPending, error} = useFetch(url)
 
-    const fetchTrips = useCallback(async () => {
-        const response = await fetch(url)
-        const json = await response.json()
-        setTrips(json)
-    },[url])
-
-    useEffect(() => {
-        fetchTrips()
-    }, [url, fetchTrips])
-
-    console.log(trips);
-
-
-    
     return (
         <div className="trip-list">
             <h2>Trip List</h2>
+            {isPending && <div>Loading trips...</div>}
+            {error && <div>{error}</div>}
             <ul>
-                {trips.map(trip => (
+                {trips && trips.map(trip => (
                     <li key={trip.id}>
                         <h3>{trip.title}</h3>
                         <p>{trip.price}</p>
